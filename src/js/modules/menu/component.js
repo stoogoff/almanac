eaa2c988dca737ad1dd3d107dfd7955f995aeac8
Stoo <stoo.goff@gmail.com>
2026-04-30 23:14:10 +0100

    Added menu as a component and display best and most recent times

---
+++ src/js/modules/menu/component.js
@@ -0,0 +1,36 @@
+
+import { local } from 'q/utils/storage.js'
+import { min, sortByProperty } from 'q/utils/list.js'
+import { formatTime } from '../../utils/number.js'
+
+export default {
+	data: {
+		games: [
+			{
+				id: 'mambo',
+				title: '’Bo',
+				strapline: 'Harmonise the grid of elements',
+				url: '/mambo.html',
+				last: false,
+				best: false,
+				showStats: false,
+			},
+		],
+	},
+
+	created() {
+		this.data.games.forEach(game => {
+			if(local.has(game.id)) {
+				const times = local.get(game.id)
+
+				if(times.length === 0) {
+					return
+				}
+
+				game.best = formatTime(times.map(({ time }) => time).reduce(min, 1000))
+				game.last = formatTime(times.sort(sortByProperty('date'))[0].time)
+				game.showStats = true
+			}
+		})
+	},
+}
