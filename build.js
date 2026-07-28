23dc316f27ca2179dadd6402555cb458b2f7fb51
Stoo <stoo.goff@gmail.com>
2026-07-28 11:16:52 +0100

    Added version number to index

--- build.js
+++ build.js
@@ -54,6 +54,7 @@ const versionHtml = async (source, target, version) => {
 				.replace(/href="css/g, `href="${version}/css`)
 				.replace(/"\.\/js/g, `"./${version}/js`)
 				.replace(/src="\/media/g, `src="${version}/media`)
+				.replace('$VERSION', version)
 
 			await Deno.writeTextFile(join(target, file.name), converted)
 		}
