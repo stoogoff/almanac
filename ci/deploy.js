26af7dfe46349b5964e0d7bb18b58e56724a83c1
Stoo <stoo.goff@gmail.com>
2026-09-18 21:03:46 +0100

    Moving build and deployment to it's own folder

---
+++ ci/deploy.js
@@ -0,0 +1,15 @@
+
+import { join } from '@std/path'
+import { load } from '@std/dotenv'
+
+// TODO 
+
+/*
+
+curl --request PUT \
+  --url https://uk.storage.bunnycdn.com/stoogoff-test/test/readme.md \
+  --header 'AccessKey: faafe761-6b45-488e-a8d1aaf7a4a5-b20a-42e6' \
+  --header 'Content-Type: application/octet-stream' \
+  --data '"<string>"'
+
+ */
